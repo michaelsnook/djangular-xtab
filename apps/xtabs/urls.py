9cc483b55a1e0522936a11633c454a056d18d7a1
michaelsnook <michael.snook@gmail.com>
2014-03-15 11:24:16 -0400

    startproject

---
+++ apps/xtabs/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+admin.autodiscover() 
+from . import views
+ 
+ 
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'xtabs.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', views.OnePageAppView.as_view(), name='home'),
+)
